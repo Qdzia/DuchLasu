0082fb89183260f9ef4936d4d9a4824e6d3fc2c4
Łukasz <qdzia.work@gmail.com>
2019-09-21 10:09:56 +0200

    Zatrzymywanie przy spadaniu
    Poprawiono moment zatrzymania przy przytrzymaniu klawisza A i D

---
+++ UnityDuchLasu/Assets/Code/CameraMovment.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovment : MonoBehaviour
+{
+
+    public Transform tr;
+    public Transform cam;
+    Vector3 pos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pos = cam.position - tr.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cam.position = tr.position;
+        cam.position += pos;
+    }
+}
--- UnityDuchLasu/Assets/Code/PlayerController.cs
+++ UnityDuchLasu/Assets/Code/PlayerController.cs
@@ -5,16 +5,21 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public Rigidbody2D rb;
-    public Transform tr;
     public GameObject player;
-    public float movmentForce = 70;
-    public float speed = 0;
-    public float maxSpeed = 1000;
-    public int jumpDuration = 50;
-    public float jumpForce = 1;
+    public float movmentForce = 0;
+    public int speed = 0;
+    public float maxSpeed = 0f;
+    public float jumpDuration = 0f;
+    public float jumpForce = 0f;
+    public float jumpLowToMax = 0f;
+    public bool canJump = false;
+    public float wallFriction=0f;
 
-    int jumpTimer = 0;
-    bool canJump = false;
+    public bool wallSideL = true;
+    public bool wallSideR = true;
+    float jumpTimer = 0f;
+    int canJumpTimer = 0;
+    float wallGravity = 0f;
     Vector2 axisX = new Vector2(1f, 0f);
     Vector2 axisY = new Vector2(0f, 1f);
     
@@ -23,51 +28,80 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        tr = GetComponent<Transform>();
+        wallGravity = rb.gravityScale;
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        speed = rb.velocity.x;
+        
+        speed = (int)rb.velocity.y;
 
         player.transform.rotation = Quaternion.Euler(Vector3.forward * 0);
+       
 
-        if (Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.D)&& wallSideR)
         {
-            if (rb.velocity.x < maxSpeed) rb.AddForce(axisX * movmentForce);
+            if (rb.velocity.x < maxSpeed) rb.AddForce(axisX * movmentForce * Time.deltaTime);
         }
 
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.A) && wallSideL)
         {
-            if (rb.velocity.x > -maxSpeed) rb.AddForce(axisX * -movmentForce);
+            if (rb.velocity.x > -maxSpeed) rb.AddForce(axisX * -movmentForce * Time.deltaTime);
         }
 
-        if (Input.GetKeyDown(KeyCode.Space)&& canJump)
-        {  
-            jumpTimer = jumpDuration;
-            Debug.Log("jump");
-        }
-        if (jumpTimer > 0)
+        if (Input.GetKeyDown(KeyCode.Space) && canJump)
         {
-            rb.AddForce(axisY * jumpForce*jumpTimer);
+            jumpTimer = 10;
+            
+
+
+        }
+
+
+        if (Input.GetKey(KeyCode.Space) && jumpTimer > 0)
+        {
+            rb.AddForce(axisY * jumpForce * Time.deltaTime);
             jumpTimer--;
-            canJump = false;
         }
 
-
+       
 
     }
-    void OnCollisionEnter2D(Collision2D col)
+    void OnCollisionStay2D(Collision2D col)
     {
-        Debug.Log("OnCollisionEnter2D");
-        if (col.gameObject.tag == "Platform" || col.gameObject.tag == "Wall") canJump = true;
-      
+       
+        if (col.gameObject.tag == "Platform" )
+        {
+            canJump = true;
+        }
+
+        if (col.gameObject.tag == "Wall")
+        {
+
+            canJump = true;
+            rb.gravityScale = 0;
+            rb.AddForce(axisY * -wallFriction * Time.deltaTime);
+
+            if (col.transform.position.x < rb.position.x) wallSideL = false;
+            else wallSideR = false;
+
+        }
+
     }
     void OnCollisionExit2D(Collision2D col)
     {
-        if (col.gameObject.tag == "Platform" || col.gameObject.tag == "Wall") canJump = false;
-        Debug.Log("OnCollisionExit2D");
+        if (col.gameObject.tag == "Platform" ) 
+        {
+            canJump = false;
+        }
+
+        if (col.gameObject.tag == "Wall")
+        {
+            canJump = false;
+            rb.gravityScale = wallGravity;
+            wallSideL = true;
+            wallSideR = true;
+        }
     }
 }
