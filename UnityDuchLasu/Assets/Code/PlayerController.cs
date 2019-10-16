04915e7faae1870021cedbf54a58b048d3ce4b85
Łukasz <qdzia.work@gmail.com>
2019-10-16 21:03:49 +0200

    Optymalizacja poruszania poziomo i skok
    Nowe metody poruszania i skoku uproszczony algorytm i wyjęcie do funkcji

--- UnityDuchLasu/Assets/Code/PlayerController.cs
+++ UnityDuchLasu/Assets/Code/PlayerController.cs
@@ -13,7 +13,7 @@ public class PlayerController : MonoBehaviour
     public int speed = 0;
     public float maxSpeed = 0f;
     public float jumpDuration = 0f;
-    public float jumpForce = 0f;
+    
     public float jumpLowToMax = 0f;
     public bool canJump = false;
     public float wallFriction=0f;
@@ -21,11 +21,23 @@ public class PlayerController : MonoBehaviour
 
     public bool wallSideL = true;
     public bool wallSideR = true;
-    float jumpTimer = 0f;
     float wallGravity = 0f;
     Vector2 axisX = new Vector2(1f, 0f);
     Vector2 axisY = new Vector2(0f, 1f);
-    
+
+
+    float moveInput;
+    public float velocity;
+    bool faceingRight = true;
+    public Transform feetPos;
+    bool isGrounded;
+    public float circleRadius;
+    public float jumpForce;
+    public float jumpTimer;
+    float jumpTimerCounter;
+    bool isJumping;
+    //public LayerMask whatIsGround; -> If want add checks in OverlapCircle
+
 
     // Start is called before the first frame update
     void Start()
@@ -37,39 +49,26 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+      //Allow to jump
+        Jump();
+
         speed = Mathf.Abs((int)rb.velocity.x);
         animator.SetFloat("Speed", speed);
-
-        if (Input.GetKeyDown(KeyCode.Space) && canJump)
-        {
-            jumpTimer = 10;
-            
-        }
+   
 
         if (transform.position.x > 10) FindObjectOfType<GameManager>().LevelCompleted();
     }
 
-    
     void FixedUpdate()
     {
-        if (Input.GetKey(KeyCode.D) && wallSideR)
-        {
-            if (rb.velocity.x < maxSpeed) rb.AddForce(axisX * movmentForce * Time.deltaTime);
-            spriteRen.flipX = false;
-        }
+      //Moving Horizontal
+        moveInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveInput * velocity, rb.velocity.y);
 
-        if (Input.GetKey(KeyCode.A) && wallSideL)
-        {
-            if (rb.velocity.x > -maxSpeed) rb.AddForce(axisX * -movmentForce * Time.deltaTime);
-            spriteRen.flipX = true;
-        }
-
-        if (Input.GetKey(KeyCode.Space) && jumpTimer > 0)
-        {
-            rb.AddForce(axisY * jumpForce * Time.deltaTime);
-            jumpTimer--;
-        }
+      //Change Faceing Side
+        Flip();
     }
+
     void OnCollisionStay2D(Collision2D col)
     {
        
@@ -98,6 +97,7 @@ public class PlayerController : MonoBehaviour
         }
 
     }
+
     void OnCollisionExit2D(Collision2D col)
     {
         if (col.gameObject.tag == "Platform" ) 
@@ -113,5 +113,43 @@ public class PlayerController : MonoBehaviour
             wallSideL = true;
             wallSideR = true;
         }
+    }
+
+    void Flip()
+    {
+      //Multiplaying scaler by negative to switch faceing side
+        Vector3 Scaler = transform.localScale;
+        if (faceingRight && moveInput < 0 || !faceingRight && moveInput > 0)
+        {
+            faceingRight = !faceingRight;
+            Scaler.x *= -1;
+        } 
+        transform.localScale = Scaler;
+    }
+
+    void Jump()
+    {
+      //Using object FeetPos placed under player to check collision with sth
+        isGrounded = Physics2D.OverlapCircle(feetPos.position, circleRadius);
+
+      //Jump on hold Method 
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.velocity = Vector2.up * jumpForce;
+            isJumping = true;
+            jumpTimerCounter = jumpTimer;
+        }
+
+        if (Input.GetKey(KeyCode.Space) && isJumping)
+        {
+            if (jumpTimerCounter > 0)
+            {
+                rb.velocity = Vector2.up * jumpForce;
+                jumpTimerCounter -= Time.deltaTime;
+            }
+            else isJumping = false;
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space)) isJumping = false;
     }
 }
