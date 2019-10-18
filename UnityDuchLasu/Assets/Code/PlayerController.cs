f4431afb3829ae78b809fd177e653f7c0dea872a
Łukasz <qdzia.work@gmail.com>
2019-10-18 20:14:56 +0200

    Optymalizacja Skok i Sciany
    Napisane na nowo funkcje skoku i odbijania się od scian

--- UnityDuchLasu/Assets/Code/PlayerController.cs
+++ UnityDuchLasu/Assets/Code/PlayerController.cs
@@ -7,35 +7,26 @@ public class PlayerController : MonoBehaviour
     public Rigidbody2D rb;
     public Player player;
     public Animator animator;
-
-    public SpriteRenderer spriteRen;
-    public float movmentForce = 0;
-    public int speed = 0;
-    public float maxSpeed = 0f;
-    public float jumpDuration = 0f;
-    
-    public float jumpLowToMax = 0f;
-    public bool canJump = false;
-    public float wallFriction=0f;
-    
-
-    public bool wallSideL = true;
-    public bool wallSideR = true;
-    float wallGravity = 0f;
-    Vector2 axisX = new Vector2(1f, 0f);
-    Vector2 axisY = new Vector2(0f, 1f);
-
-
-    float moveInput;
-    public float velocity;
-    bool faceingRight = true;
     public Transform feetPos;
-    bool isGrounded;
+    public LayerMask mask;
+
+    public int speed = 0;
+    public float velocity;
+    public float wallFriction;
+    public int extraJumpsNumber;
     public float circleRadius;
     public float jumpForce;
     public float jumpTimer;
+    public int extraJumps;
+
+    float moveInput;
+    bool faceingRight = true;
+    bool isGrounded;
     float jumpTimerCounter;
     bool isJumping;
+    int wallID;
+    bool blockDir;
+    
     //public LayerMask whatIsGround; -> If want add checks in OverlapCircle
 
 
@@ -43,101 +34,100 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        wallGravity = rb.gravityScale;
     }
 
     // Update is called once per frame
     void Update()
     {
-      //Allow to jump
+        //Allow to jump
         Jump();
 
         speed = Mathf.Abs((int)rb.velocity.x);
         animator.SetFloat("Speed", speed);
-   
+
 
         if (transform.position.x > 10) FindObjectOfType<GameManager>().LevelCompleted();
     }
 
     void FixedUpdate()
     {
-      //Moving Horizontal
+        //Moving Horizontal
         moveInput = Input.GetAxis("Horizontal");
-        rb.velocity = new Vector2(moveInput * velocity, rb.velocity.y);
+        if(BlocDir())rb.velocity = new Vector2(moveInput * velocity, rb.velocity.y);
 
-      //Change Faceing Side
+        //Change Faceing Side
         Flip();
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Enemy")
+        {
+            rb.AddForce(new Vector2(9f, 15f), ForceMode2D.Impulse);
+            player.hp = -20;
+        }
+
+        //Add extra jump after colision with wall, check id to not allow on second jump on the same wall
+        if (col.gameObject.tag == "Wall")
+        {
+            blockDir = true;
+
+            if (wallID != col.gameObject.GetInstanceID() && extraJumps==0)
+            {
+                extraJumps++;
+                Debug.Log("nierówne");
+                wallID = col.gameObject.GetInstanceID();
+            }
+
+            Debug.Log("zmien sciane");
+        }
+
     }
 
     void OnCollisionStay2D(Collision2D col)
     {
-       
-        if (col.gameObject.tag == "Platform" )
-        {
-            canJump = true;
-            animator.SetBool("Ground", true);
-        }
-
+        //Stable velocity with wall contact
         if (col.gameObject.tag == "Wall")
         {
-
-            canJump = true;
-            rb.gravityScale = 0;
-            rb.AddForce(axisY * -wallFriction * Time.deltaTime);
-
-            if (col.transform.position.x < rb.position.x) wallSideL = false;
-            else wallSideR = false;
-
+            if (rb.velocity.y < -wallFriction) rb.velocity = Vector2.down * wallFriction;
         }
-
-        if (col.gameObject.tag == "Enemy")
-        {
-            rb.AddForce(new Vector2(9f, 15f),ForceMode2D.Impulse);
-            player.hp = -20;
-        }
-
     }
 
     void OnCollisionExit2D(Collision2D col)
     {
-        if (col.gameObject.tag == "Platform" ) 
-        {
-            canJump = false;
-            animator.SetBool("Ground", false);
-        }
-
-        if (col.gameObject.tag == "Wall")
-        {
-            canJump = false;
-            rb.gravityScale = wallGravity;
-            wallSideL = true;
-            wallSideR = true;
-        }
+        if (col.gameObject.tag == "Wall") blockDir = false;
     }
 
     void Flip()
     {
-      //Multiplaying scaler by negative to switch faceing side
+        //Multiplaying scaler by negative to switch faceing side
         Vector3 Scaler = transform.localScale;
         if (faceingRight && moveInput < 0 || !faceingRight && moveInput > 0)
         {
             faceingRight = !faceingRight;
             Scaler.x *= -1;
-        } 
+        }
         transform.localScale = Scaler;
     }
 
     void Jump()
     {
-      //Using object FeetPos placed under player to check collision with sth
-        isGrounded = Physics2D.OverlapCircle(feetPos.position, circleRadius);
+        //Using object FeetPos placed under player to check collision with sth
+        isGrounded = Physics2D.OverlapCircle(feetPos.position, circleRadius,mask);
+        if (isGrounded)
+        {
+            extraJumps = extraJumpsNumber;
+            wallID = 0;
+        } 
 
-      //Jump on hold Method 
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        //Jump on hold Method + multiple Jump 
+        if (Input.GetKeyDown(KeyCode.Space) && extraJumps > 0)
         {
             rb.velocity = Vector2.up * jumpForce;
             isJumping = true;
             jumpTimerCounter = jumpTimer;
+            Debug.Log("Jump");
+            extraJumps--;
         }
 
         if (Input.GetKey(KeyCode.Space) && isJumping)
@@ -151,5 +141,17 @@ public class PlayerController : MonoBehaviour
         }
 
         if (Input.GetKeyUp(KeyCode.Space)) isJumping = false;
+
+    }
+
+    bool BlocDir()
+    {
+        if (blockDir)
+        {
+            if (faceingRight && moveInput > 0) return false;
+            else if(!faceingRight && moveInput < 0) return false;
+        }
+
+        return true;
     }
 }
