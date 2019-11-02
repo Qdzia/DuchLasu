fc6bfbbbcf4c762c3ba6ef47d945ef646781fc9e
Łukasz <qdzia.work@gmail.com>
2019-11-02 16:39:49 +0100

    Statystyki
    wersja testowa niedokonczona

---
+++ UnityDuchLasu/Assets/Code/Stats/PlayerStats.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : CharacterStats
+{
+    void Start()
+    {
+        EquipmentManager.instance.onEquipmentChanged += OnEquipmentChanged;
+    }
+
+    void OnEquipmentChanged(Equipment newItem, Equipment oldItem)
+    {
+
+    }
+}
---
+++ UnityDuchLasu/Assets/Code/Stats/Stat.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public class Stat
+{
+    [SerializeField]
+    private int baseValue =0;
+
+    private List<int> modifires = new List<int>();
+    public int GetValue()
+    {
+        return baseValue;
+    }
+
+    public void AddModifire(int modifire)
+    {
+        if (modifire != 0)
+        {
+            modifires.Add(modifire);
+        }
+    }
+
+    public void RemoveModifire(int modifire)
+    {
+        if (modifire != 0)
+        {
+            modifires.Add(modifire);
+        }
+    }
+
+    //8 15
+}
